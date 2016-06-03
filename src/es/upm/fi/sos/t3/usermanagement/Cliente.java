35290e173ad98bfd58151fa8cda796adcb57b608
Ángel <angel7delcastillo@gmail.com>
2016-06-03 20:19:30 +0200

    Cest fini

--- src/es/upm/fi/sos/t3/usermanagement/Cliente.java
+++ src/es/upm/fi/sos/t3/usermanagement/Cliente.java
@@ -6,7 +6,7 @@ import es.upm.fi.sos.t3.usermanagement.UserManagementWSStub.*;
 
 public class Cliente {
 
-	public static void main(String[] args) throws RemoteException {
+	public static void main(String[] args) throws RemoteException, InterruptedException {
 		
 		UserManagementWSStub um = new UserManagementWSStub();
 		um._getServiceClient().engageModule("addressing");
@@ -27,36 +27,56 @@ public class Cliente {
 		ppair.setOldpwd("user1");
 		ppair.setNewpwd("asdf");
 			
+		Response res;
 		
-		System.out.println("Ejecutando cliente. La lista actual de usuarios es "
-		+ um.verUsuarios());
+		System.out.println("Ejecutando cliente de prueba...");
+		Thread.sleep(2000);
 		
 		System.out.println("Login de superuser");
-		um.login(superuser);
-		
+		res = um.login(superuser);
+		System.out.println(res.getResponse());
+		System.out.println("");
+		Thread.sleep(2000);
 		
 		System.out.println("Añadir el usuario user1");
-		um.addUser(user1);
+		res = um.addUser(user1);
+		System.out.println(res.getResponse());
+		System.out.println("");
+		Thread.sleep(2000);
 		
 		System.out.println("Logout de superuser");
 		um.logout();
+		System.out.println("");
+		Thread.sleep(2000);
 		
 		System.out.println("Login de user1");
-		um.login(user1);
+		res = um.login(user1);
+		System.out.println(res.getResponse());
+		System.out.println("");
+		Thread.sleep(2000);
 		
-		System.out.println("El usuario user1 cambia su contraseña a la más segura asdf");
-		um.changePassword(ppair);
-		
+		System.out.println("El usuario user1 cambia su contraseña a la más segura (?)  \"asdf\"");
+		res = um.changePassword(ppair);
+		System.out.println(res.getResponse());
+		System.out.println("");
+		Thread.sleep(2000);
 		
 		System.out.println("Logout de user 1");
 		um.logout();
+		System.out.println("");
+		Thread.sleep(2000);
 		
 		System.out.println("Login de superuser");
-		um.login(superuser);
+		res = um.login(superuser);
+		System.out.println(res.getResponse());
+		System.out.println("");
+		Thread.sleep(2000);
 		
 		System.out.println("Borrar el usuario user1");
-		um.removeUser(username1);
-		
+		res = um.removeUser(username1);
+		System.out.println(res.getResponse());
+		System.out.println("");
+		Thread.sleep(2000);
 		
 		System.out.println("Logout de superuser");
 		um.logout();
